e514c10237d56d3871a0570fbaf5ccfd016ffa49
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2025-02-10 23:21:46 +0300

    Hesap oluşturma sayısının kontrol edilmesi ve hesaba eklenen katılımcı sayısının kontrol edilmesi geliştirmeleri yapıldı

--- src/ButceYonet.Application/Application/Shared/UserPlanRuleValidators/NotebookCountRuleValidator.cs
+++ src/ButceYonet.Application/Application/Shared/UserPlanRuleValidators/NotebookCountRuleValidator.cs
@@ -4,13 +4,29 @@ using ButceYonet.Application.Domain.Enums;
 using ButceYonet.Application.Infrastructure.Data;
 using DotBoil.Caching;
 using DotBoil.EFCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace ButceYonet.Application.Application.Shared.UserPlanRuleValidators;
 
 public class NotebookCountRuleValidator : IUserPlanRuleValidator
 {
+    private readonly IRepository<Notebook, ButceYonetDbContext> _notebookRepository;
+
+    public NotebookCountRuleValidator(IRepository<Notebook, ButceYonetDbContext> notebookRepository)
+    {
+        _notebookRepository = notebookRepository;
+    }
+    
     public async Task<bool> Validate(int userId, PlanFeature planFeature, IDictionary<string, string> parameters)
     {
-        return true;
+        var notebookCount = await _notebookRepository
+            .GetAll()
+            .Where(u => u.NotebookUsers.Any(y => y.UserId == userId))
+            .CountAsync();
+        
+        if (planFeature.Count > notebookCount)
+            return true;
+        
+        return false;
     }
 }
--- src/ButceYonet.Application/Application/Shared/UserPlanRuleValidators/NotebookUserCountRuleValidator.cs
+++ src/ButceYonet.Application/Application/Shared/UserPlanRuleValidators/NotebookUserCountRuleValidator.cs
@@ -4,13 +4,35 @@ using ButceYonet.Application.Domain.Enums;
 using ButceYonet.Application.Infrastructure.Data;
 using DotBoil.Caching;
 using DotBoil.EFCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace ButceYonet.Application.Application.Shared.UserPlanRuleValidators;
 
 public class NotebookUserCountRuleValidator : IUserPlanRuleValidator
 {
+    private readonly IRepository<NotebookUser, ButceYonetDbContext> _notebookUserRepository;
+
+    public NotebookUserCountRuleValidator(IRepository<NotebookUser, ButceYonetDbContext> notebookUserRepository)
+    {
+        _notebookUserRepository = notebookUserRepository;
+    }
+    
     public async Task<bool> Validate(int userId, PlanFeature planFeature, IDictionary<string, string> parameters)
     {
-        return true;
+        if (!parameters.ContainsKey("NotebookId"))
+            return true;
+
+        if (!int.TryParse(parameters["NotebookId"], out var notebookId))
+            return true;
+
+        var notebookUserCount = await _notebookUserRepository
+            .GetAll()
+            .Where(p => p.NotebookId == notebookId)
+            .CountAsync();
+
+        if (planFeature.Count > notebookUserCount)
+            return true;
+        
+        return false;
     }
 }
