9df2598d06779425926bb83f4dc104dfd7887183
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2025-11-25 19:46:35 +0300

    User id bilgisinin tokenden çekilmesi

--- src/ButceYonet.Application/Infrastructure/Services/User.cs
+++ src/ButceYonet.Application/Infrastructure/Services/User.cs
@@ -1,8 +1,19 @@
+using System.IdentityModel.Tokens.Jwt;
 using ButceYonet.Application.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace ButceYonet.Application.Infrastructure.Services;
 
 public class User : IUser
 {
     public int Id { get; }
+
+    public User(IHttpContextAccessor httpContextAccessor)
+    {
+        var sub = httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(c => c.Type.EndsWith("nameidentifier")).Value ??
+            string.Empty;
+
+        int.TryParse(sub, out int _id);
+        Id = _id; 
+    }
 }
