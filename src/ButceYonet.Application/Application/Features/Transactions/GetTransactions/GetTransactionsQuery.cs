255d16e11cde55f301745cb1079c42382d992fa3
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2026-04-18 01:48:33 +0300

    Update GetTransactionsQuery.cs

--- src/ButceYonet.Application/Application/Features/Transactions/GetTransactions/GetTransactionsQuery.cs
+++ src/ButceYonet.Application/Application/Features/Transactions/GetTransactions/GetTransactionsQuery.cs
@@ -9,8 +9,8 @@ public class GetTransactionsQuery : PaginationFilter, IRequest<BaseResponse>
     public int NotebookId { get; set; }
     public DateTime? StartTime { get; set; }
     public DateTime? EndTime { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
     public decimal Amount { get; set; }
     public TransactionTypes? TransactionType { get; set; }
     public List<int> LabelIds { get; set; }
